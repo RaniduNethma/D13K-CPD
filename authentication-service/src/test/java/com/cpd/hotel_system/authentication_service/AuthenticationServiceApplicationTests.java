341a516461a107d834f62c22ff2375bc646fc7e8
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-03-14 19:40:12 +0530

    Initialize Authentication Service
    Initialize Authentication Service

---
+++ authentication-service/src/test/java/com/cpd/hotel_system/authentication_service/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpd.hotel_system.authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
